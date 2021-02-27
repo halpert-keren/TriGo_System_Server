4ae695aaefac407955b9a52ed41ec03fac79c629
halpert-keren <halpertkeren@gmail.com>
2021-02-27 13:40:18 +0200

    requests

---
+++ models/request.js
@@ -0,0 +1,10 @@
+const { Schema, model } = require('mongoose');
+
+const requestSchema = new Schema({
+    groupID: { type: String, required: true },
+    ownerID: { type: String, required: true },
+    requesterID: { type: String, required: true },
+}, { collection: 'requests' });
+
+const Request = model('Request', requestSchema);
+module.exports = Request;
