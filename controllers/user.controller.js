4f54524854d9a58e52fff5ff87ec15b7aa3a4827
opalpeltzman <opalpeltzman@gmail.com>
2021-02-28 11:42:51 +0200

    update

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -8,7 +8,7 @@ getUsers = (req, res) => {
 }
 
 getUser = (req, res) => {
-    User.findOne({_id: req.params.id})
+    User.findOne({googleID: req.params.id})
         .then(docs => res.json(docs))
         .catch(err => console.log(err))
 }
@@ -40,22 +40,41 @@ createUser = (token, req, res) => {
 
 updateUser = (req, res) => {
     const { body } = req
-    if (body.savedTrails != null) {
-        const itemToAdd = body.savedTrails[0]
-        User.updateOne({_id: req.params.id}, {$push: {savedTrails: itemToAdd}})
-            .then(() => res.json({id: `${req.params.id}`}))
-            .catch(err => console.log(err))
+    if(body.action)
+    {
+        if (body.savedTrails != null) {
+            const itemToAdd = body.savedTrails[0]
+            User.updateOne({googleID: req.params.id}, {$push: {savedTrails: itemToAdd}})
+                .then(() => {
+                    User.findOne({googleID: req.params.id})
+                        .then(docs => res.json(docs))
+                        .catch(err => console.log(err))
+                })
+                .catch(err => console.log(err))
+        }
     }
-    else if (body.groups != null) {
-        const itemToAdd = body.groups[0]
-        User.updateOne({_id: req.params.id}, {$push: {groups: itemToAdd}})
-            .then(() => res.json({id: `${req.params.id}`}))
-            .catch(err => console.log(err))
+    else{
+        if (body.savedTrails != null) {
+            const itemToDelete = body.savedTrails[0]
+            User.updateOne({googleID: req.params.id}, {$pull: {savedTrails: itemToDelete}})
+                .then(() => {
+                    User.findOne({googleID: req.params.id})
+                        .then(docs => res.json(docs))
+                        .catch(err => console.log(err))
+                })
+                .catch(err => console.log(err))
+        }
     }
+    // else if (body.groups != null) {
+    //     const itemToAdd = body.groups[0]
+    //     User.updateOne({_id: req.params.id}, {$push: {groups: itemToAdd}})
+    //         .then(() => res.json({id: `${req.params.id}`}))
+    //         .catch(err => console.log(err))
+    // }
 }
 
 deleteUser = (req, res) => {
-    User.deleteOne({_id: req.params.id})
+    User.deleteOne({googleID: req.params.id})
         .then(() => res.json({success: true}))
         .catch(err => console.log(err))
 }
